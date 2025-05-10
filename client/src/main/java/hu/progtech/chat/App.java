94e059134f900b5c0ccd923253c59abd2dd62174
matee8 <graves-bluff-pesky@duck.com>
2025-05-10 10:52:35 +0200

    chore: add client and server subproject

--- client/src/main/java/hu/progtech/chat/App.java
+++ client/src/main/java/hu/progtech/chat/App.java
@@ -1,5 +1,8 @@
 package hu.progtech.chat;
 
+/**
+ * Hello world!
+ */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
---
+++ server/src/main/java/hu/progtech/chat/App.java
@@ -0,0 +1,10 @@
+package hu.progtech.chat;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
