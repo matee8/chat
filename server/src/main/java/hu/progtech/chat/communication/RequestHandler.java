4e85c0d2596fbbc0b862303474d7ea64386f5ccf
matee8 <graves-bluff-pesky@duck.com>
2025-05-14 19:52:38 +0200

    feat(server): add gRPC service definitions

--- server/src/main/java/hu/progtech/chat/communication/RequestHandler.java
+++ server/src/main/java/hu/progtech/chat/communication/RequestHandler.java
@@ -1,8 +1,9 @@
 package hu.progtech.chat.communication;
 
+import hu.progtech.chat.proto.ChatServiceGrpc;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class RequestHandler {
+public class RequestHandler extends ChatServiceGrpc.ChatServiceImplBase {
     private static final Logger logger = LogManager.getLogger(RequestHandler.class);
 }
